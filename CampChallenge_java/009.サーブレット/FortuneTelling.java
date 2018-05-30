7cdc7e1cdd978acb4f50a7db5fe63d085ed518e7
DESKTOP-5P4AR38\Ken Hanaoka <ken.hanaoka99@gmail.com>
2018-05-30 16:19:59 +0900

    Javaサーブレット課題を提出します．

---
+++ CampChallenge_java/009.サーブレット/FortuneTelling.java
@@ -0,0 +1,72 @@
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Ken Hanaoka
+ */
+
+public class FortuneTelling extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            // 11種類の要素を配列luckListに格納し，ランダム要素番号を決めて要素を取り出して表示する//
+            String luckList[] = {"大吉","中吉","小吉","吉","半吉","末半吉","凶","小凶","半凶","末凶","大凶"};
+            Random rand = new Random();
+            Integer index = rand.nextInt(luckList.length);
+            out.println("あなたの運勢は" + luckList[index]);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
