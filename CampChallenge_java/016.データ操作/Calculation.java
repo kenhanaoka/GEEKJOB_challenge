4ecd57a708a272e57f8e73b1cf5abf1ae2c9d6d6
DESKTOP-5P4AR38\Ken Hanaoka <ken.hanaoka99@gmail.com>
2018-06-10 00:31:50 +0900

    Java課題(クエリストリングの利用)を提出します．

---
+++ CampChallenge_java/016.データ操作/Calculation.java
@@ -0,0 +1,76 @@
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Ken Hanaoka
+ */
+public class Calculation extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            request.setCharacterEncoding("UTF-8");
+            double total = Double.valueOf(request.getParameter("total"));
+            int typeNum = Integer.valueOf(request.getParameter("type"));
+            double count = Double.valueOf(request.getParameter("count"));
+            String[] type = {"雑貨","生鮮食品","その他"};
+            out.println(type[typeNum-1]);
+            out.println("単価 " + total/count);
+            if(total > 5000) {
+                out.println("ポイント " + total*0.05);
+            }
+            else if(total > 3000) {
+                out.println("ポイント " + total*0.04);
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
